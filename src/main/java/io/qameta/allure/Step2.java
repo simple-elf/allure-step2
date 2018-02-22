7cdec328035c8ceda381114812815d75b4e77c78
Aaftakhov <simple1elf@gmail.com>
2018-02-22 11:20:44 +0500

    Step2

---
+++ src/main/java/io/qameta/allure/Step2.java
@@ -0,0 +1,31 @@
+package io.qameta.allure;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotations to mark step methods.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Step2 {
+
+    /**
+     * The step text.
+     *
+     * @return the step text.
+     */
+    String value() default "";
+
+    /**
+     * Option for hide step parameters.
+     *
+     * @return boolean flag to disable step parameters showing.
+     */
+    boolean hideParams() default true;
+
+}
---
+++ src/main/java/io/qameta/allure/aspects/Steps2Aspects.java
@@ -0,0 +1,75 @@
+package io.qameta.allure.aspects;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureLifecycle;
+import io.qameta.allure.Step2;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StepResult;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import static io.qameta.allure.util.AspectUtils.getParameters;
+import static io.qameta.allure.util.AspectUtils.getParametersMap;
+import static io.qameta.allure.util.NamingUtils.processNameTemplate;
+import static io.qameta.allure.util.ResultsUtils.getStatus;
+import static io.qameta.allure.util.ResultsUtils.getStatusDetails;
+
+/**
+ * @author Dmitry Baev charlie@yandex-team.ru
+ *         Date: 24.10.13
+ * @author sskorol (Sergey Korol)
+ */
+@Aspect
+public class Steps2Aspects {
+
+    private static AllureLifecycle lifecycle;
+
+    @SuppressWarnings("PMD.UnnecessaryLocalBeforeReturn")
+    @Around("@annotation(io.qameta.allure.Step2) && execution(* *(..))")
+    public Object step(final ProceedingJoinPoint joinPoint) throws Throwable {
+        final MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        final Step2 step = methodSignature.getMethod().getAnnotation(Step2.class);
+        final String name = step.value().isEmpty()
+                ? methodSignature.getName()
+                : processNameTemplate(step.value(), getParametersMap(methodSignature, joinPoint.getArgs()));
+        final String uuid = UUID.randomUUID().toString();
+        final StepResult result = new StepResult()
+                .withName(name);
+        if (!step.hideParams())
+            result.withParameters(getParameters(methodSignature, joinPoint.getArgs()));
+        getLifecycle().startStep(uuid, result);
+        try {
+            final Object proceed = joinPoint.proceed();
+            getLifecycle().updateStep(uuid, s -> s.withStatus(Status.PASSED));
+            return proceed;
+        } catch (Throwable e) {
+            getLifecycle().updateStep(uuid, s -> s
+                    .withStatus(getStatus(e).orElse(Status.BROKEN))
+                    .withStatusDetails(getStatusDetails(e).orElse(null)));
+            throw e;
+        } finally {
+            getLifecycle().stopStep(uuid);
+        }
+    }
+
+    /**
+     * For tests only.
+     *
+     * @param allure allure lifecycle to set.
+     */
+    public static void setLifecycle(final AllureLifecycle allure) {
+        lifecycle = allure;
+    }
+
+    public static AllureLifecycle getLifecycle() {
+        if (Objects.isNull(lifecycle)) {
+            lifecycle = Allure.getLifecycle();
+        }
+        return lifecycle;
+    }
+}
